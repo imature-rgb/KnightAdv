be40327c323144f18b3f78eba428903af5b114f0
imature-rgb <imature95@gmail.com>
2024-11-22 03:53:21 +0100

    1

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public class Test : MonoBehaviour
+{
+    private void Update()
+    {
+        printCons();
+    }
+    
+    private void printCons() {
+        Debug.Log("Hello World");
+    }
+}
